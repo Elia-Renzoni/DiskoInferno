91b11ff24e6bc5c910696167f8e9054fb208447d
Elia Renzoni <elia.renzoni03@gmail.com>
2026-05-06 08:29:41 +0200

    refactor: buffer pool and page structure
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- src/buffer_pool_manager.cpp
+++ src/buffer_pool_manager.cpp
@@ -1,1 +1,37 @@
+#include <mutex>
 
+#include "buffer_pool_manager.hpp"
+
+
+void BufferPoolManager::bootBufferPoolManager() {
+    auto maxBlocks = BufferPoolManager::maxPages;
+    for (int i = 0; i < maxBlocks; i++) {
+        auto emptyPage = Page();
+        BufferPoolManager::bufferPool.push_back(emptyPage);
+    }
+}
+
+void BufferPoolManager::WritePage(const int pid, const char *data) {
+    /*
+    auto result = BufferPoolManager::findPage(pid);
+    Page *page;
+    if (result.has_value()) {
+        page = result.value();
+    }
+
+    page->pinPage();
+    std::unique_lock lock(page->latch);
+    // TODO-> replacing policy and write operation
+    //
+    // for eviction:
+
+
+    page->unpinPage();
+    */
+}
+
+void BufferPoolManager::DeletePage(const int pid) {
+}
+
+void BufferPoolManager::ReadPage(const int pid) {
+}
--- src/disk_manager.hpp
+++ src/disk_manager.hpp
@@ -4,4 +4,6 @@
 #pragma once
 
 class DiskManager : public Interface {
+    public:
+        DiskManager();
 };
